b3566ecb0860fd8d383a2db360a9af4ef4ed5ec1
ZakyMalika <zakymalika0987@gmail.com>
2025-11-28 15:40:42 +0700

    membuat file Repository siswa di package repositori dengan menggunakan inheritance dari class RepositoriSiswa.kt

--- app/src/main/java/com/example/roomdb/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdb/repositori/RepositoriSiswa.kt
@@ -1,2 +1,18 @@
 package com.example.roomdb.repositori
 
+import com.example.roomdb.room.Siswa
+import com.example.roomdb.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa{
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+) : RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
