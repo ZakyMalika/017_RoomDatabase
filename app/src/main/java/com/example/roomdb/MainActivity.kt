2364fcef362bfc2ef5468c2a70096cb164f7b125
ZakyMalika <zakymalika0987@gmail.com>
2025-11-28 18:31:44 +0700

    membuat file MainActivity.kt

--- app/src/main/java/com/example/roomdb/MainActivity.kt
+++ app/src/main/java/com/example/roomdb/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.roomdb.ui.theme.RoomDBTheme
+import com.example.roomdb.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             RoomDBTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
+//                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
