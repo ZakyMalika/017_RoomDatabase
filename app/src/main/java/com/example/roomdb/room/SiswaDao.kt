e3fe293dbc44be01eb2c14769e93c125f96379bf
ZakyMalika <zakymalika0987@gmail.com>
2025-11-28 15:23:37 +0700

    membuat data access object untuk data siswa

--- app/src/main/java/com/example/roomdb/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdb/room/SiswaDao.kt
@@ -1,4 +1,18 @@
 package com.example.roomdb.room
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface SiswaDao {
+    @Query("SELECT * FROM tblSsiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+
+
 }
