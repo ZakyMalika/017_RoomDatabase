85882c39d7b4e8981152761f01d4e29a96725f47
ZakyMalika <zakymalika0987@gmail.com>
2025-11-28 18:30:57 +0700

    membuat file DestinasiEntry.kt

---
+++ app/src/main/java/com/example/roomdb/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.roomdb.view.route
+
+import com.example.roomdb.R
+
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
