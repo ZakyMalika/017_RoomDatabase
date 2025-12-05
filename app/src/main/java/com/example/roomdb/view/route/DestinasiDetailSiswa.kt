b188cebafe7c64437b02b773ed9e7bb651ac1b68
ZakyMalika <zakymalika0987@gmail.com>
2025-12-05 12:59:32 +0700

    membuat object destinasi detail

--- app/src/main/java/com/example/roomdb/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/roomdb/view/route/DestinasiDetailSiswa.kt
@@ -1,1 +1,11 @@
 package com.example.roomdb.view.route
+
+
+import com.example.roomdb.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
