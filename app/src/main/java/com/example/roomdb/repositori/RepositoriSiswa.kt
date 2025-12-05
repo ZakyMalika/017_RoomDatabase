7bb143be94f84e5f16a54e272f6ccc2343234e7c
ZakyMalika <zakymalika0987@gmail.com>
2025-12-05 13:03:54 +0700

    menambah reository siswa

--- app/src/main/java/com/example/roomdb/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdb/repositori/RepositoriSiswa.kt
@@ -7,12 +7,13 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa{
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
+//    suspend fun SearchSiswa(nama: String): List<Siswa>
+//    suspend fun SearchSiswa(nama: String): Flow<Siswa>
+
 }
 
-class OfflineRepositoriSiswa(
-    private val siswaDao: SiswaDao
-) : RepositoriSiswa {
-    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
-
-    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
-}
