0a3610ae632a8fedd3726130fbe2ce449e2cc24a
ZakyMalika <zakymalika0987@gmail.com>
2025-12-05 13:04:02 +0700

    menambah repsitory siswa

--- app/src/main/java/com/example/roomdb/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdb/repositori/RepositoriSiswa.kt
@@ -17,3 +17,14 @@ interface RepositoriSiswa{
 
 }
 
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+) : RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+
+}
